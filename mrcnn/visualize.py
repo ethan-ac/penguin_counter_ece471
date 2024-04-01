84ea7811b4c4ee0312c92c8981b1e92f652838de
ethan-ac <ethanclarke365@gmail.com>
2024-04-01 13:32:11 -0700

    change result header font size

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -117,10 +117,8 @@ def display_instances(image, boxes, masks, class_ids, class_names,
     height, width = image.shape[:2]
     ax.set_ylim(height + 10, -10)
     ax.set_xlim(-10, width + 10)
-    # ax.axis('off')
-    ax.axis('on')
-    ax.set_xlabel(title, fontname='Comic Sans MS', fontsize=20)
-    ax.set_title(title, fontname='Comic Sans MS')
+    ax.axis('off')
+    ax.set_title(title, fontsize=20)
 
     masked_image = image.astype(np.uint32).copy()
     for i in range(N):
