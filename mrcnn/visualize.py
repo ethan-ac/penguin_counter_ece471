c49dc55bede7b2fb691fb034cc38e50140f0eb1e
ethan-ac <ethanclarke365@gmail.com>
2024-04-01 13:51:37 -0700

    change mask text colour test

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -20,8 +20,6 @@ from matplotlib import patches,  lines
 from matplotlib.patches import Polygon
 import IPython.display
 
-from termcolor import colored
-
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../")
 
@@ -142,7 +140,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             class_id = class_ids[i]
             score = scores[i] if scores is not None else None
             label = class_names[class_id]
-            caption = colored("{} {:.3f}".format(label, score) if score else label, 'red')
+            caption = "{} {:.3f}".format(label, score) if score else label
         else:
             caption = captions[i]
         ax.text(x1, y1 + 8, caption,
