04c3565718e09f6f306b7e4fe06394793a2675c9
ethan-ac <ethanclarke365@gmail.com>
2024-04-01 13:17:33 -0700

    remove test change colour of mask text

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -140,7 +140,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             class_id = class_ids[i]
             score = scores[i] if scores is not None else None
             label = class_names[class_id]
-            caption = "\033[0;31m {} {:.3f}".format(label, score) if score else label
+            caption = "{} {:.3f}".format(label, score) if score else label
         else:
             caption = captions[i]
         ax.text(x1, y1 + 8, caption,
